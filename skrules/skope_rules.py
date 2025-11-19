5705c0a397ddf75e7dca0ef227ef8b3b99c54f96
Scott Elmore <selmore@prosper.com>
2025-11-19 11:06:08 -0600

    Update import for indices_to_mask utility

--- skrules/skope_rules.py
+++ skrules/skope_rules.py
@@ -8,7 +8,7 @@ from warnings import warn
 from sklearn.base import BaseEstimator
 from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import check_classification_targets
-from sklearn.utils._indexing import indices_to_mask
+from sklearn_compat.utils._mask import indices_to_mask
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 from sklearn.ensemble import BaggingClassifier, BaggingRegressor
 from sklearn.tree import _tree
